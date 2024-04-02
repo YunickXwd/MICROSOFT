1849899d90e33e0e3d1c422b9e7af07d6b4315a3
UXRXT-CYBER <128343127+UNIKBAAP@users.noreply.github.com>
2024-04-02 10:26:20 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 import os
-print(" Checking for update");os.system('git pull');os.system('python3 .MICROSOFT UNLIMITED ACC CREATE/microsoftacc.py')
+print(" Checking for update");os.system('git pull');os.system('python3 .MICROSOFTUNLIMITEDACCCREATE/microsoftacc.py')
